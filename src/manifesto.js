5b7758783776bdc6bf8d9ca83de5289afc30391c
samjgorman <samjgorman@gmail.com>
2020-07-26 20:07:47 -0700

    copy changes

--- src/manifesto.js
+++ src/manifesto.js
@@ -8,14 +8,14 @@ import Line from "./components/line";
 function Manifesto() {
   return (
     <div className="Manifesto">
-    <Line text = "Assistants were supposed to help students like us.  Seriously.">
+    <Line text = "Assistants were supposed to help students like you.  Seriously.">
     </Line>
 
     <Line text = "But, we never quite got Jarvis from Iron Man or a futuristic VR headset.">
     </Line>
 
 
-    <Paragraph text = "Instead, Siri tells you what it found on the web.  Alexa tells Jeff Bezos what you found on the web.  We all expected more from these products.  You were a kid when they were first released.  And we placed our wonder and excitement into imagining a future where our technology was crafted to help us be our best selves."> 
+    <Paragraph text = "Instead, Siri tells you what it found on the web.  Alexa tells Jeff Bezos what you found on the web.  You expected more from these products.  You were a kid when they were first released.  And you placed wonder and excitement into imagining a future where our technology was crafted to help us be our best self."> 
     </Paragraph>
 
     <Line text = "You learned to live with the products you had, and over time, the term personal assistant got a bad rap.">
@@ -24,7 +24,7 @@ function Manifesto() {
     <Paragraph text = "Now, an assistant sounds more like a fancy butler than something you can see yourself needing.  On top of that, you're busier than you ever were before.  The word has lost its touch.  Its lost the joy, and even if you had the time to care, you wouldn't. "> 
     </Paragraph>
 
-    <Paragraph text = "Iris is a second chance to fall in love with the word assistant.   It's how it should have been built from the beginning, dead simple, and its deeply yours.  Its your starter park for your life as a student, pandemic and all. "> 
+    <Paragraph text = "Iris is a second chance to fall in love with the word assistant.   It's how it should have been built from the beginning, dead simple, and its deeply yours.  Its your starter pack for your life as a student, pandemic and all. "> 
     </Paragraph>
 
     <Line text = "And we're starting somewhere you spend a lot of time in.  The kitchen, making food for yourself and others.">
