403fd05172366449e0614b7d3cf7d6263e2b573e
samjgorman <samjgorman@gmail.com>
2020-08-27 21:16:38 -0700

    test

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
 
 
     <Title text = "An Extension of You"></Title>
-    <Subtitle text = "A leap forward for college students to think in new ways."></Subtitle>
+    <Subtitle text = "A leap forward for college students to win community back."></Subtitle>
     <a href = "https://irisapp.typeform.com/to/aER0lIzm" target = "_blank"> <ConfirmButton text = "Join the Waitlist"></ConfirmButton> </a> 
 
     <Divider></Divider>
