7360d0f3fafd81f327eb8593c036105a8cbe5711
samjgorman <samjgorman@gmail.com>
2020-09-02 00:04:11 -0700

    pp

--- src/index.js
+++ src/index.js
@@ -2,14 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Privacy from './privacy';
+
 import * as serviceWorker from './serviceWorker';
+import { useLocation, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { withRouter } from "react-router-dom";
+
+
+class ScrollToTop extends React.Component {
+  componentDidUpdate(prevProps) {
+    if (
+      this.props.location.pathname !== prevProps.location.pathname
+    ) {
+      window.scrollTo(0, 0);
+    }
+  }
+
+  render() {
+    return null;
+  }
+}
+
+export default withRouter(ScrollToTop);
+
+
+const routing = (
+    <Router>
+      <div>
+      {/* <ScrollToTop/>  */}
+
+        <Switch> 
+
+        <Route exact path="/" component={App} />
+        <Route path="/privacy" component={Privacy} />
+  
+
+        </Switch>
+      </div>
+    </Router>
+  )
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+routing
+,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
