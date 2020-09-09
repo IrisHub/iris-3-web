dca59662a541f166ffde7b669730b721e6ad5a28
samjgorman <samjgorman@gmail.com>
2020-09-09 02:50:51 -0700

    copy shorten

--- src/manifesto.js
+++ src/manifesto.js
@@ -18,23 +18,23 @@ function Manifesto() {
     <Paragraph text = "But students our age faced this problem before. In the 1960s, young thinkers and activists birthed a counterculture that rejected the same bleak feeling you feel now. They fought for a world that gave them hope. They protested on the streets and rejected the demands of an older generation.  Then, they cast technology out of stuffy research labs and into the hands of the people."> 
     </Paragraph>
 
-    <Line text = "These students demanded that technology shouldn't just be a cold instrument, but that it should be a powerful tool to help you be your best, most enlightened self.">
+    <Line text = "These students demanded that technology shouldn't just be a cold instrument, but that it should be a powerful tool to help you be your best, brightest self.">
     </Line>
 
-    <Paragraph text = "Now the products and services that you use don't inspire you anymore. The apps on your phone spy on you. Your most personal information is sold everyday. And when was the last time Facebook or Microsoft released a product that inspired you?"> 
+    <Paragraph text = "Now the products you use don't inspire you anymore.  So we're extending the ideas of past counterculture thinkers to hijack technology for a second time. We believe that technology should be an extension of you."> 
     </Paragraph>
 
     <Line text = "So we're extending the ideas of past counterculture thinkers to hijack technology for a second time. We believe that technology should be an extension of you.">
     </Line>
 
-    <Line text = "And as a first step, we're starting with a set of tools for your life as a remote student — an iOS app that helps you reclaim your new identity in the online world, with the people you care about.">
+    <Line text = "And as a first step, we're starting with an iOS app that helps you make the most of your time in online school.">
     </Line>
 
-    <Line text = "We're a small team of student activists, hackers, scientists, artists and thinkers that have come together to challenge the status quo.">
+    <Line text = "If you believe that this should be built, sign up for our waitlist for early access.  You'll be among the first to get your hands on Iris and shape the direction we're headed.">
     </Line>
 
-    <Line text = "We're asking you to challenge this with us — but we could use your help.  We're building just for upperclassmen at UC Berkeley for now — because we're students there too. Sign up for our waitlist today!">
-    </Line>
+    {/* <Line text = "We're asking you to challenge this with us — but we could use your help.  We're building just for upperclassmen at UC Berkeley for now — because we're students there too. Sign up for our waitlist today!">
+    </Line> */}
 
 
     </div>
